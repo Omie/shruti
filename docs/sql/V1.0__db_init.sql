9b926288f7a6346f289c9041961a2124362e411f
omie <intelomkar@gmail.com>
2015-07-11 14:47:35 +0530

    updated schema
    - rename plugins to providers, suits better
    - map a setting to a provider

--- docs/sql/V1.0__db_init.sql
+++ docs/sql/V1.0__db_init.sql
@@ -5,13 +5,14 @@ SET SCHEMA 'shrutidb';
 
 
 CREATE TABLE "settings" (
-    "id"    SERIAL PRIMARY KEY,
-    "key"   TEXT UNIQUE NOT NULL,
-    "value" jsonb NOT NULL
+    "id"        SERIAL PRIMARY KEY,
+    "key"       TEXT UNIQUE NOT NULL,
+    "value"     JSONB NOT NULL,
+    "provider"  INTEGER NOT NULL
 );
 
 
-CREATE TABLE "plugins" (
+CREATE TABLE "providers" (
     "id"            SERIAL PRIMARY KEY,
     "name"          TEXT UNIQUE NOT NULL,
     "display_name"  TEXT NOT NULL,
@@ -22,13 +23,14 @@ CREATE TABLE "plugins" (
 );
 
 CREATE TABLE "notifications" (
-    "id"        SERIAL PRIMARY KEY,
-    "title"     TEXT NOT NULL,
-    "url"       TEXT,
-    "key"       TEXT UNIQUE NOT NULL,
-    "provider"  INTEGER NOT NULL,
-    "created_on" timestamp without time zone NOT NULL DEFAULT now()
+    "id"            SERIAL PRIMARY KEY,
+    "title"         TEXT NOT NULL,
+    "url"           TEXT,
+    "key"           TEXT UNIQUE NOT NULL,
+    "provider"      INTEGER NOT NULL,
+    "created_on"    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()
 );
 
-ALTER TABLE "notifications" ADD CONSTRAINT notifications_fk_provider FOREIGN KEY (provider) REFERENCES plugins(id);
+ALTER TABLE "settings" ADD CONSTRAINT settings_fk0 FOREIGN KEY (provider) REFERENCES providers(id);
+ALTER TABLE "notifications" ADD CONSTRAINT notifications_fk_provider FOREIGN KEY (provider) REFERENCES providers(id);
 
