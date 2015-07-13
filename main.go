96283d35d01f048db2b1ebf4846185af1514d100
omie <intelomkar@gmail.com>
2015-07-13 14:07:33 +0530

    cleanup. get rid of ivona from api server

--- main.go
+++ main.go
@@ -19,18 +19,6 @@ import (
 
 func main() {
 
-	/*
-		responseQueue = NewTTSResponseQueue()
-
-		accessKey := os.Getenv("SHRUTI_ACCESSKEY")
-		secretKey := os.Getenv("SHRUTI_SECRETKEY")
-		if accessKey == "" || secretKey == "" {
-			log.Println("main: environment variables not set")
-			return
-		}
-
-		InitIvona(accessKey, secretKey)
-	*/
 	dbUser := os.Getenv("SHRUTI_DBUSER")
 	dbName := os.Getenv("SHRUTI_DBNAME")
 	if dbUser == "" || dbName == "" {
@@ -44,7 +32,6 @@ func main() {
 
 	api.InitSwagger(fmt.Sprintf("http://%s:%d", "127.0.0.1", 9574))
 
-	// go RunResponseWorker()
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", 9574),
 		Handler: api.Container,
